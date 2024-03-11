073b4386d16520216322da7ea1c687dd4bdd89a4
dittko <wiktorm205@gmail.com>
2024-03-11 18:38:01 +0100

    Implementacja obsługi formatów obsługi formatów XLSX i CSV

--- DataSources/dataSources.py
+++ DataSources/dataSources.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+import pandas as pd
 
 class IDataSource(metaclass = ABCMeta):
     @classmethod
@@ -7,19 +8,40 @@ class IDataSource(metaclass = ABCMeta):
                 callable(subclass.GetData))
         
     @abstractmethod
-    def GetData():
+    def GetData(self):
         raise RuntimeError
     
 class DatabaseHandler(IDataSource):
     def __init__(self) -> None:
         pass
     
-    def GetData():
+    def GetData(self):
         pass
     
-class FileInputHandler(IDataSource):
-    def __init__(self) -> None:
-        pass
+class XLSXHandler(IDataSource):
+    def __init__(self, path: str) -> None:
+        self.file_path = path
     
-    def GetData():
-        pass
+    def GetData(self) -> pd.DataFrame:
+        """otwiera plik XLS"""
+        try:
+            data = pd.read_excel(self.file_path)
+            return data
+        except Exception as e:
+            print("Błąd podczas wczytywania pliku XLSX:", e)
+            return None
+
+
+class CSVXHandler(IDataSource):
+    def __init__(self, path: str) -> None:
+        self.file_path = path
+        
+    
+    def GetData(self) -> pd.DataFrame:
+        """otwiera plik csv"""
+        try:
+            data = pd.read_csv(self.file_path)
+            return data
+        except Exception as e:
+            print("Błąd podczas wczytywania pliku CSV:", e)
+            return None
