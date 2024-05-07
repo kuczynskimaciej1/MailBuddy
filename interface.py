40fa5311b2ba6b7b1bf955305cb394f4eb6ca27e
Kacper Hałaczkiewicz <54312917+ikarmus2001@users.noreply.github.com>
2024-05-07 22:47:30 +0200

    Dodanie podglądu na bieżąco w TemplateEditorze

--- interface.py
+++ interface.py
@@ -1,14 +1,15 @@
 from collections.abc import Callable, Iterable
+from enum import Enum
 from sqlalchemy.exc import IntegrityError
 from types import TracebackType
 from traceback import print_tb
 from typing import Literal, Any, NoReturn
-from tkinter import Menu, simpledialog, ttk, Listbox, Tk, Text, Button, Frame, Label, Entry, Scrollbar, Toplevel, Misc, messagebox, Menubutton, Canvas, VERTICAL, RAISED
+from tkinter import Event, Menu, simpledialog, ttk, Listbox, Tk, Text, Button, Frame, Label, Entry, Scrollbar, Toplevel, Misc, messagebox, Menubutton, Canvas, VERTICAL, RAISED
 from tkinter.ttk import Combobox
 from tkinter.constants import NORMAL, DISABLED, BOTH, RIDGE, END, LEFT, RIGHT, TOP, X, Y, INSERT, SEL, WORD
 from group_controller import GroupController
 from models import Contact, IModel, Template, Group
-from tkhtmlview import HTMLLabel
+from tkhtmlview import HTMLLabel, HTMLText
 from DataSources.dataSources import GapFillSource
 
 def errorHandler(xd, exctype: type, excvalue: Exception, tb: TracebackType):
@@ -350,32 +351,45 @@ class TemplateEditor(Toplevel):
 
         name_label = Label(self, text="Nazwa szablonu:", bg="lightblue")
         name_entry = Entry(self, bg="white", fg="black")
-        template_text = Text(self, bg="lightblue", fg="black", wrap=WORD)
+        
+        # ids = StringVar(value="")
+        self.template_text = HTMLText(self, bg="lightblue", fg="black", wrap=WORD) # , textvariable=ids
+        self.template_text.bind("<KeyRelease>", self.__on_html_key_clicked)
+        self.template_text.bind("<<TextModified>>", self.__on_text_changed)
+        # ids.trace_add("w", self.__on_text_changed)
+        
+        self.template_preview = HTMLLabel(self, bg="lightblue", fg="black", wrap=WORD)
         btn_save = Button(self, text="Zapisz", bg="lightblue", fg="black", command=lambda: self.__save_template_clicked(
-            name_entry.get(), template_text.get(1.0, END)))
+            name_entry.get(), self.template_text.get(1.0, END)))
         btn_insert_placeholder = Button(self, text="Wstaw luke", bg="lightblue", fg="black",
-                                        command=lambda: self.__template_window_insert_placeholder(template_text))
+                                        command=self.__template_window_insert_placeholder)
+        
         
         name_label.grid(row=0, column=0, padx=5, pady=5)
         name_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
-        template_text.grid(row=1, column=0, columnspan=2,
+        self.template_text.grid(row=1, column=0, columnspan=2,
                            padx=5, pady=5, sticky="nsew")
+        self.template_preview.grid(row=1, column=3, columnspan=5, padx=5, pady=5, sticky="nsew")
         btn_save.grid(row=2, column=0, padx=5, pady=5, sticky="e")
         btn_insert_placeholder.grid(
             row=2, column=1, padx=5, pady=5, sticky="w")
         
-        
-        testGap = GapFillSource()
-        testGap.get_possible_values()
-        if self.currentTemplate:
+        if self.currentTemplate: # if anything is present in template
             name_entry.insert(
                 INSERT,
                 self.currentTemplate.name if self.currentTemplate.name is not None else "")
             
-            template_text.insert(
+            self.template_text.insert(
                 INSERT,
                 self.currentTemplate.content if self.currentTemplate.content is not None else "")
-            
+            self.template_text.event_generate("<<TextModified>>") # Initial render
+    
+    def __on_html_key_clicked(self, event: Event):
+        if event.keycode not in NonAlteringKeyCodes:
+            self.template_text.event_generate("<<TextModified>>")
+
+    def __on_text_changed(self, event):
+        self.template_preview.set_html(self.template_text.get("1.0", END))
 
     def __save_template_clicked(
             self, template_name: str, template_content: str) -> None:
@@ -390,24 +404,22 @@ class TemplateEditor(Toplevel):
             self.current_combo.destroy()
 
     def __template_window_insert_placeholder(
-            self, template_text: str, placeholders: list[GapFillSource] = GapFillSource.all_instances) -> None:
+            self, placeholders: list[GapFillSource] = GapFillSource.all_instances) -> None:
         placeholder_text = "_____"
-        # combo_values = [key for key in [gap.possible_values.keys() for gap in placeholders]]
         combo_values = [key for placeholder in placeholders for key in placeholder.possible_values]
-
 
         def on_placeholder_selection(event):
             selected_placeholder = self.current_combo.get()
             if selected_placeholder:
-                selected_text = template_text.tag_ranges(SEL)
+                selected_text = self.template_text.tag_ranges(SEL)
                 if selected_text:
-                    template_text.delete(selected_text[0], selected_text[1])
-                template_text.insert(INSERT, selected_placeholder)
+                    self.template_text.delete(selected_text[0], selected_text[1])
+                self.template_text.insert(INSERT, selected_placeholder)
 
         def show_placeholder_menu(event):
             self.hide_combobox()
             self.current_combo = Combobox(
-                template_text, values=combo_values)
+                self.template_text, values=combo_values)
             #TODO: Debug populating combobox
             self.current_combo.bind(
                 "<<ComboboxSelected>>", on_placeholder_selection)
@@ -418,20 +430,20 @@ class TemplateEditor(Toplevel):
                 self.current_combo, text="X", command=self.hide_combobox, bg="white")
             close_button.place(relx=0, rely=0, anchor="nw")
 
-        template_text.insert(INSERT, placeholder_text)
-        template_text.tag_configure("placeholder", background="lightgreen")
+        self.template_text.insert(INSERT, placeholder_text)
+        self.template_text.tag_configure("placeholder", background="lightgreen")
 
-        template_text.bind("<Button-3>", show_placeholder_menu)
+        self.template_text.bind("<Button-3>", show_placeholder_menu)
 
         start_index = "1.0"
         while True:
-            start_index = template_text.search(
+            start_index = self.template_text.search(
                 placeholder_text, start_index, stopindex=END)
             if not start_index:
                 break
-            end_index = template_text.index(
+            end_index = self.template_text.index(
                 f"{start_index}+{len(placeholder_text)}c")
-            template_text.tag_add("placeholder", start_index, end_index)
+            self.template_text.tag_add("placeholder", start_index, end_index)
             start_index = end_index
 
 class GroupEditor(Toplevel):
@@ -614,3 +626,31 @@ class AddContactWindow(Toplevel):
             # TODO: Jakiś sygnał do parenta żeby się zaktualizował?
         else:
             messagebox.showerror("Błąd", "Podaj adres e-mail")
+
+class NonAlteringKeyCodes(Enum):
+    # List is non-exhaustive, should be tested
+    # via https://asawicki.info/nosense/doc/devices/keyboard/key_codes.html
+    
+    # Backspace = 8
+    # Tab = 9
+    Num_Key_5 = 12
+    # Enter = 13
+    Shift = 16
+    Ctrl = 17
+    Alt = 18
+    Pause_Break = 19
+    Caps_Lock = 20
+    Esc = 27
+    # Space = 32
+    Page_Up = 33
+    Page_Down = 34
+    End = 35
+    Home = 36
+    Left_Arrow = 37
+    Up_Arrow = 38
+    Right_Arrow = 39
+    Down_Arrow = 40
+    Print_Screen = 44
+    Insert = 45
+    # Delete = 46
+
